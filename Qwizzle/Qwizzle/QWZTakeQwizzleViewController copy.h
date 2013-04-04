73553068a5b6c6ba411f1ae26ff6eb8055304875
Baneen <baneeen@hotmail.com>
2013-04-04 14:14:27 -0400

    syn request
    trying to syn , I had " Rebase Conflicts"

---
+++ Qwizzle/Qwizzle/QWZTakeQwizzleViewController copy.h
@@ -0,0 +1,28 @@
+//
+//  QWZAnswerViewController.h
+//  Qwizzle
+//
+//  Created by Krissada Dechokul on 3/22/13.
+//  Copyright (c) 2013 Florida Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class QWZQuizSet;
+@class QWZAnsweredQuizSet;
+@class QWZQwizzleViewController;
+
+@interface QWZTakeQwizzleViewController : UIViewController
+{
+    IBOutlet UIScrollView *scrollView;
+}
+
+@property (nonatomic, weak) QWZQwizzleViewController *origin;
+@property (nonatomic, strong) NSMutableArray *answerList;
+@property (nonatomic, strong) NSMutableArray *controlList;
+@property (nonatomic, strong) QWZQuizSet *quizSet;
+@property (nonatomic, strong) QWZAnsweredQuizSet *answeredQuizSet;
+
+- (IBAction)fillOutAQwizzle:(id)sender;
+
+
+@end

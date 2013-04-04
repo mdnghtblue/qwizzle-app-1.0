e30c59a685d532aedc587f008a6592548446f8ac
Baneen <baneeen@hotmail.com>
2013-04-03 22:02:38 -0400

    Revert "Revert "Revert "Revert "I did the database tables, triggers...etc.""""
    This reverts commit f3f7627aab4d4207a5fbb0e08196f6b7e7c3d7b7.

--- Qwizzle/Qwizzle/QWZAnsweredQuizSet.m
+++ Qwizzle/Qwizzle/QWZAnsweredQuizSet.m
@@ -8,6 +8,7 @@
 
 #import "QWZAnsweredQuizSet.h"
 
+
 @implementation QWZAnsweredQuizSet
 
 @end
--- Qwizzle/Qwizzle/QWZViewView.h
+++ Qwizzle/Qwizzle/QWZViewView.h
@@ -1,12 +1,12 @@
 //
 //  QWZViewView.h
 //  Qwizzle
-//
 //  Copyright (c) 2013 Florida Tech. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface QWZViewView : UIViewController
+
 
 @end

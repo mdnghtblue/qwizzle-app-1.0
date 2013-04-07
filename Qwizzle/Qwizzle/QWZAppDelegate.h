6ee3e381c2b0b72077d56bce198330437cc5119e
Krissada Dechokul <krissada_d@hotmail.com>
2013-04-07 17:44:04 -0400

    Remove Core data related stuffs

--- Qwizzle/Qwizzle/QWZAppDelegate.h
+++ Qwizzle/Qwizzle/QWZAppDelegate.h
@@ -10,13 +10,5 @@
 
 @interface QWZAppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
-
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
 
 @end
